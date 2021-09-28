c0af2cf189346bcaccfa155b44e8583705972194
Abdul Khan <abdulkhan@FVFFN4A3Q05P.local>
2021-09-28 18:20:47 +0530

    2waychat working fine

--- examples/publisher.js
+++ examples/publisher.js
@@ -1,11 +1,8 @@
 ((red5prosdk) => {
-
+   
     const publisher = new red5prosdk.RTCPublisher();
     const sendButton = document.getElementById('send');
     const SharedObject = red5prosdk.Red5ProSharedObject;
-    
-    
-   
 
     const configuration = {
         // protocol: "ws",
@@ -15,8 +12,7 @@
         port: 443,
         host: "red5stream.searceinc.org",
         app: "live",
-        streamName1: 'mystream1',
-        streamName2: 'mystream2',
+        streamName: "mystream",
         rtcConfiguration: {
             iceServers: [{urls: "stun:stun2.l.google.com:19302"}],
             iceCandidatePoolSize: 2,
@@ -45,15 +41,25 @@
         },
     }
 
+const joinmyclassButton = document.getElementById("publishTeacher");
+const startPublishingTeacherStream = () => {
     publisher.init(configuration).then(() => {
         console.log('LOG :: Publish Init Done');
         publisher.publish();
+        
     }).then(() => {
         console.log('LOG :: Publish Done');
         establishSharedObject(publisher);
     }).catch((error) => {
         console.log('ERROR :: ' + error);
     });
+}
+joinmyclassButton.addEventListener('click', () => {
+    startPublishingTeacherStream();
+    var T = document.getElementById("media-screen");
+    T.style.display = "flex";
+    
+});
 
     
 
@@ -66,6 +72,19 @@
         const soField = document.getElementById(input.id);
         soField.value = ['User "' + message.user + '": ' + message.message];
     }
+
+    // const streamTransmit=(stream)=>{
+    //     try {
+    //         console.log("pub" +stream)
+    //         var divStream = document.getElementById("stream-publisher");
+    //         var video = document.createElement("my_video");
+    //         video.srcObject = stream;
+    //         divStream.appendChild(video);
+    //         video.play();
+    //     } catch (err) {
+    //         console.log('error :: ' + err)
+    //     }
+    // }
     
 
     const establishSharedObject = (publisher) => {
@@ -73,6 +92,9 @@
         const soCallback = {
             messageTransmit: messageTransmit
         };
+        // const soCallbackVideoStream={
+        //     streamTransmit: streamTransmit
+        // };
 
         so.on(red5prosdk.SharedObjectEventTypes.CONNECT_SUCCESS, (event) => {
             console.log('[Red5ProPublisher] SharedObject Connect.');
@@ -82,18 +104,87 @@
         so.on(red5prosdk.SharedObjectEventTypes.CONNECT_FAILURE, (event) => {
             console.log('[Red5ProPublisher] SharedObject Fail.');
         });
-
+        so.on(red5prosdk.SharedObjectEventTypes.CONNECTION_CLOSED, (event) => {
+            console.log('[Red5ProPublisher] SharedObject connection closed.');
+        });
+        so.on(red5prosdk.SharedObjectEventTypes.PUBLISH_INVALID_NAME, (event) => {
+            console.log('[Red5ProPublisher]'+ configuration.streamName+'provided is already in use');
+        });
+        
         so.on(red5prosdk.SharedObjectEventTypes.PROPERTY_UPDATE, (event) => {
             console.log('[Red5ProPublisher] SharedObject Property Update.');
-            console.log(JSON.stringify(event.data, null, 2));
+            // console.log(JSON.stringify(event.data, null, 2));
         });
 
         so.on(red5prosdk.SharedObjectEventTypes.METHOD_UPDATE, (event) => {
             console.log('[Red5ProPublisher] SharedObject Method Update.');
             console.log(JSON.stringify(event.data, null, 2));
             soCallback[event.data.methodName].call(null, event.data.message);
+            // soCallbackVideoStream[event.data.methodName].call( event.data.stream);
         });
     }
+    // var hasRegistered=false;
+    // function establishSharedObject (publisher, roomName, streamName) {
+    //     // Create new shared object.
+    //     so = new SharedObject(roomName, publisher)
+    //     so.on(red5prosdk.SharedObjectEventTypes.CONNECT_SUCCESS, function (event) { // eslint-disable-line no-unused-vars
+    //       console.log('[Red5ProPublisher] SharedObject Connect.');
+    //       console.log('Connected.');
+    //     });
+    //     so.on(red5prosdk.SharedObjectEventTypes.CONNECT_FAILURE, function (event) { // eslint-disable-line no-unused-vars
+    //       console.log('[Red5ProPublisher] SharedObject Fail.');
+    //     });
+    //     so.on(red5prosdk.SharedObjectEventTypes.PROPERTY_UPDATE, function (event) {
+    //       console.log('[Red5ProPublisher] SharedObject Property Update.');
+    //       console.log(JSON.stringify(event.data, null, 2));
+    //       if (event.data.hasOwnProperty('streams')) {
+    //         console.log('Stream list is: ' + event.data.streams + '.');
+    //         var streams = event.data.streams.length > 0 ? event.data.streams.split(',') : [];
+    //         if (!hasRegistered) {
+    //           hasRegistered = true;
+    //           so.setProperty('streams', streams.concat([streamName]).join(','));
+    //         }
+    //         streamsPropertyList = streams;
+    //         processStreams(streamsPropertyList, streamName);
+    //       }
+    //       else if (!hasRegistered) {
+    //         hasRegistered = true;
+    //         streamsPropertyList = [streamName];
+    //         so.setProperty('streams', streamName);
+    //       }
+    //     });
+    //   }
+
+    //   function processStreams (streamlist, exclusion) {
+    //     var nonPublishers = streamlist.filter(function (name) {
+    //       return name !== exclusion;
+    //     });
+    //     var list = nonPublishers.filter(function (name, index, self) {
+    //       return (index == self.indexOf(name)) &&
+    //         !document.getElementById(window.getConferenceSubscriberElementId(name));
+    //     });
+    //     var subscribers = list.map(function (name, index) {
+    //       return new window.ConferenceSubscriberItem(name, subscribersEl, index);
+    //     });
+    //     var i, length = subscribers.length - 1;
+    //     var sub;
+    //     for(i = 0; i < length; i++) {
+    //       sub = subscribers[i];
+    //       sub.next = subscribers[sub.index+1];
+    //     }
+    //     if (subscribers.length > 0) {
+    //       var baseSubscriberConfig = Object.assign({},
+    //                                   configuration,
+    //                                   {
+    //                                     protocol: getSocketLocationFromProtocol().protocol,
+    //                                     port: getSocketLocationFromProtocol().port
+    //                                   },
+    //                                   getAuthenticationParams(),
+    //                                   getUserMediaConfiguration());
+    //       subscribers[0].execute(baseSubscriberConfig);
+    //     }
+    //   }
+
 
     sendButton.addEventListener('click', () => {
         const message = document.getElementById('text-area').value;
@@ -105,42 +196,85 @@
             })
         }
     });
-    // var captureButton = document.getElementById('capture-button');
-    // const videoTag = document.getElementById('red5pro-publisher');
 
-    // async function capture (cb) {
-    //     captureButton.disabled = true;
-    //     var config = {
-    //         audio: true,
-    //         video:true,
-    //         video: {
-    //           width: 640, //{ maxWidth: vw },
-    //           height: 480, //{ maxHeight: vh },
-    //           frameRate: 20//{ maxFrameRate: fr }
-    //         }
-    //     };
-    //     console.log("before")
-    //     console.log('Using Capture Configuration:\r\n' + JSON.stringify(config, null, 2));
-    //     // Edge has getDisplayMedia on navigator and not media devices?
-    //     var p = undefined
-    //     if (navigator.getDisplayMedia) {
-    //       p = await navigator.getDisplayMedia(config)
-    //     } else {
-    //       p = await navigator.mediaDevices.getDisplayMedia(config)
-    //     }
-    //     console.log(p)
-    //     console.log("after")
-        
-    //     return p
-    //   }
+    //start subscribing from here:
 
-    // captureButton.addEventListener('click', function() {
-        
-    //     p=capture(publisher);
-    //     videoTag.srcObject = p.id;
-    //     videoTag.onloadedmetadata = function(e) {
-    //     videoTag.play();
-    //     };
-    //   });
-    // console.log(subscriber.getPeerConnection())
+
+
+
+
+
+
+
+
+    const subscriber = new red5prosdk.RTCSubscriber();
+    var video = document.createElement("video");
+    video.id="red5pro-subscriber"+Math.random().toString(16).slice(2);
+ 
+    const SubscriberConfiguration = {
+        protocol: "wss",
+        port: 443,
+        host: "red5stream.searceinc.org",
+        // protocol: 'ws',
+        // port: 5080,
+        // host: 'localhost',
+        app: 'live',
+        streamName: "mystream2",
+        rtcConfiguration: {
+            iceServers: [{urls: 'stun:stun2.l.google.com:19302'}],
+            iceCandidatePoolSize: 2,
+            bundlePolicy: 'max-bundle'
+        },
+        mediaElementId: video.id,
+        publishingId: 'mystream2' + Math.floor(Math.random() * 0x10000).toString(16),
+        videoEncoding: 'NONE',
+        audioEncoding: 'NONE',
+        rtcpMuxPolicy: 'negotiate',
+        }
+
+       // Initialize
+const showStudentButton = document.getElementById("subscribeStudents");
+const startSubscribingStudentStream = () => {
+    subscriber.init(SubscriberConfiguration).then(() => {
+        console.log('LOG :: Subscribe Init Done')
+        subscriber.subscribe();
+    
+    }).then(() => {
+        console.log('LOG :: Subscribe Done')
+    }).catch((error) => {
+        console.log('ERROR :: ' + error);
+    });
+}
+showStudentButton.addEventListener('click', () => {
+
+    var divStream = document.getElementById("media-screen");
+    var div = document.createElement('div');
+    div.className="stream";
+  
+    video.setAttribute("autoplay",true);
+    video.setAttribute("controls",true);
+    video.setAttribute("muted",true);
+    video.setAttribute("class","red5pro-media");
+    video.setAttribute("class","red5pro-media-background");
+    div.append(video);
+    divStream.append(div);
+
+    startSubscribingStudentStream();
+    var T = document.getElementById("media-screen");
+    T.style.display = "flex";
+
+});
+
+
+
+
+
+
+
+
+
+
+
+  
+    
 })(window.red5prosdk);
--- examples/subscriber.js
+++ examples/subscriber.js
@@ -2,7 +2,9 @@
     const subscriber = new red5prosdk.RTCSubscriber();
     const SharedObject = red5prosdk.Red5ProSharedObject;
     const sendButton = document.getElementById('send');
-
+    // const streamButton = document.getElementById('sendStream');
+    var so=undefined;
+    
     const configuration = {
         protocol: "wss",
         port: 443,
@@ -11,8 +13,7 @@
         // port: 5080,
         // host: 'localhost',
         app: 'live',
-        streamName1: 'mystream1',
-        streamName2: 'mystream2',
+        streamName: "mystream",
         rtcConfiguration: {
             iceServers: [{urls: 'stun:stun2.l.google.com:19302'}],
             iceCandidatePoolSize: 2,
@@ -23,22 +24,33 @@
         videoEncoding: 'NONE',
         audioEncoding: 'NONE',
         rtcpMuxPolicy: 'negotiate',
+        }
 
-    }
+
 
     // Initialize
-    subscriber.init(configuration).then(() => {
-        console.log('LOG :: Subscribe Init Done')
-        subscriber.subscribe();
-    }).then(() => {
-        console.log('LOG :: Subscribe Done')
-        establishSharedObject(subscriber);
-    }).catch((error) => {
-        console.log('ERROR :: ' + error);
-    });
-
+const showTeacherButton = document.getElementById("subscribeTeacher");
+    const startSubscribingTeacherStream = () => {
+        subscriber.init(configuration).then(() => {
+            console.log('LOG :: Subscribe Init Done')
+            subscriber.subscribe();
+        
+        }).then(() => {
+            console.log('LOG :: Subscribe Done')
+            establishSharedObject(subscriber);
+        }).catch((error) => {
+            console.log('ERROR :: ' + error);
+        });
+}
+showTeacherButton.addEventListener('click', () => {
+    startSubscribingTeacherStream();
+    var T = document.getElementById("media-screen");
+    T.style.display = "flex";
+    
+});
 
     const messageTransmit = (message) => {
+        console.log("sub" +message)
         var div = document.getElementById("messages");
         var input = document.createElement("textarea");
         input.id = "so-field" + Math.random().toString(16).slice(2)
@@ -47,7 +59,6 @@
         const soField = document.getElementById(input.id);
         soField.value = ['User "' + message.user + '": ' + message.message];
     }
-
     const establishSharedObject = (subscriber) => {
         try {
             so = new SharedObject('sharedObjectTest', subscriber);
@@ -58,53 +69,117 @@
         const soCallback = {
             messageTransmit: messageTransmit
         };
-        // var hasRegistered=false;
+        
         so.on(red5prosdk.SharedObjectEventTypes.CONNECT_SUCCESS, (event) => {
             console.log('[Red5ProSubscriber] SharedObject Connect.');
-            window.alert("connected")
-            
+            window.alert("connected"); 
         });
-
+       
         so.on(red5prosdk.SharedObjectEventTypes.CONNECT_FAILURE, (event) => {
             console.log('[Red5ProSubscriber] SharedObject Fail.');
         });
-
+        so.on(red5prosdk.SharedObjectEventTypes.CONNECTION_CLOSED, (event) => {
+            console.log('[Red5ProSubscriber] SharedObject connection closed.');
+        });
+        
         so.on(red5prosdk.SharedObjectEventTypes.PROPERTY_UPDATE, (event) => {
             console.log('[Red5ProPublisher] SharedObject Property Update.');
-            console.log(JSON.stringify(event.data, null, 2));
-            // if (event.data.hasOwnProperty('count')) {
-            //     console.log('User count is: ' + event.data.count + '.')
-            //     if (!hasRegistered) {
-            //       hasRegistered = true
-            //       so.setProperty('count', parseInt(event.data.count) + 1)
-            //     }
-            //   }
-            //   else if (!hasRegistered) {
-            //     hasRegistered = true
-            //     so.setProperty('count', 1)
-            //   }
-        });
+    });
 
         so.on(red5prosdk.SharedObjectEventTypes.METHOD_UPDATE, function (event) {
             console.log('[Red5ProPublisher] SharedObject Method Update.');
             console.log(JSON.stringify(event.data, null, 2));
+            console.log(event.data.methodName);
             soCallback[event.data.methodName].call(null, event.data.message);
+        
         });
+    };
 
-    }
+    
 
     sendButton.addEventListener('click', function () {
         let message = document.getElementById('text-area').value
         if (message != undefined) {
             console.log(message);
+            console.log("sub" +"before send function")
             so.send('messageTransmit', {
                 user: "Student",
                 message: message
             });
         }
     });
-    // console.log(subscriber.getPeerConnection())
 
+    //start publishing from here:
+    const publisher = new red5prosdk.RTCPublisher();
+    var video = document.createElement("video");
+    video.id="red5pro-publisher"+Math.random().toString(16).slice(2);
+    const configurationPublisher = {
+        // protocol: "ws",
+        // port: 5080,
+        // host: "localhost",
+        protocol: "wss",
+        port: 443,
+        host: "red5stream.searceinc.org",
+        app: "live",
+        streamName: "mystream2",
+        rtcConfiguration: {
+            iceServers: [{urls: "stun:stun2.l.google.com:19302"}],
+            iceCandidatePoolSize: 2,
+            bundlePolicy: "max-bundle",
+        },
+        streamMode: "live",
+        mediaElementId: video.id,
+        bandwidth: {
+            audio: 56,
+            video: 512,
+        },
+        mediaConstraints: {
+            audio: true,
+            video: {
+                width: {
+                    exact: 640,
+                },
+                height: {
+                    exact: 480,
+                },
+                frameRate: {
+                    min: 8,
+                    max: 24,
+                },
+            },
+        },
+    }
+    const joinmyclassButton = document.getElementById("publishStudent");
+    const startPublishingStudentStream = () => {
+        publisher.init(configurationPublisher).then(() => {
+            console.log('LOG :: Publish Init Done');
+            publisher.publish();
+            
+        }).then(() => {
+            console.log('LOG :: Publish Done');
+        }).catch((error) => {
+            console.log('ERROR :: ' + error);
+        });
+    }
+    joinmyclassButton.addEventListener('click', () => {
 
+   
+    var divStream = document.getElementById("media-screen");
+    var div = document.createElement('div');
+    div.className="stream";
+    video.setAttribute("autoplay",true);
+    video.setAttribute("controls",true);
+    video.setAttribute("muted",true);
+    video.setAttribute("class","red5pro-media");
+    video.setAttribute("class","red5pro-media-background");
+    div.append(video);
+    divStream.append(div);
+
+        startPublishingStudentStream();
+        var T = document.getElementById("media-screen");
+        T.style.display = "flex";
+        
+    });
+    
 
 })(window.red5prosdk);
